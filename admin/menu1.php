2c9fe150c36955264176ed09f146360fb393ceb3
Dewan Rahadyan <dewan.rahadyan@yahoo.co.id>
2017-01-06 17:10:20 +0700

    Hapus tiga menu di popup admin disamping kanan atas

--- admin/menu1.php
+++ admin/menu1.php
@@ -1,14 +1,1 @@
-                                <li class="user-body">
-                                    <!--div class="col-xs-4 text-center">
-                                        <a href="departement.php">Depart</a>
-                                    </div-->
-                                    <div class="col-xs-4 text-center">
-                                        <a href="karyawan.php">Karyawan</a>
-                                    </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="gaji.php">Penggajian</a>
-                                    </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="admin.php">Admin</a>
-                                    </div>
-                                </li>
+                 
